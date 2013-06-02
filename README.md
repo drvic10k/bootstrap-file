b7e421128eca522bafc7c142ac9cf5af1259bf36
drvic10k <matus.brlit@gmail.com>
2013-06-02 10:28:08 -0700

    Added optional attributes

--- README.md
+++ README.md
@@ -6,10 +6,17 @@ file upload input styled with bootstrap
 To use, you need minimum markup, all styling is done using css and javascript.
 
 ```html
-<input type="file" name="anyName" id="anyId" class="file" data-span="3" />
+<input type="file" name="anyName" id="anyId" class="file" data-span="3" data-file="1" />
 ```
 
 `class` is needed to identify the element for styling,
 `data-span` is for sizing, the value `x` means, that the file input will have class `spanx` (in this case `span3`).
+`data-file` is for distinguishing several file inputs on one page.
 
 Styling is done in document.ready event callback, if you want to add elements later, just call `$.bootstrapFile` afted adding them.
+
+Optional attributes:
+
+`title` ["Open"] : defines the text displayed on the Open button, if ommited, default text is "Open"
+`data-orientation` ["right"] : defines position of the Open button, if not specified, default position is to the right of the input box
+`data-remove` ["false"] : if "true", control has appended button for removing selected file (functionality not yet implemented)
--- js/bootstrap-file.js
+++ js/bootstrap-file.js
@@ -1,24 +1,33 @@
 ﻿(function () {
-	var $document = $(document);
+    var $document = $(document);
 
-	$.bootstrapFile = function () {
-		$("input.file[type='file']").each(function (index) {
-			$this = $(this);
-			$this.addClass("supressedfile span" + $this.attr("data-span")).attr("data-file", index).on("change", function (e) {
-				$this = $(e.target);
-				var index = $this.attr("data-file");
-				$("[data-file='" + index + "'][type='text']").val($this.val().replace(/^.*[\\\/]/, ''));
-			});
-			var fileinputs = $("<div>").addClass("fileinputs span" + $this.attr("data-span"));
-			var fakeinput = $("<div>").addClass("input-append fakefile");
-			var input = $("<input class='span" + ($this.attr("data-span") - 1).toString() + "' type='text' data-file='" + index + "'>");
-			var button = $("<button class='btn'>Open</button>");
-			fakeinput.append(input, button);
-			$this.parent().append(fileinputs);
-			$this.appendTo(fileinputs);
-			fileinputs.append(fakeinput);
-		});
-	};
+    $.bootstrapFile = function () {
+        $("input.file[type='file']").each(function (index) {
+            $this = $(this);
+            $this.addClass("supressedfile span" + $this.attr("data-span").toString()).attr("data-file", index).on("change", function (e) {
+                $this = $(e.target);
+                var index = $this.attr("data-file");
+                $("[data-file='" + index + "'][type='text']").val($this.val().replace(/^.*[\\\/]/, ''));
+            });
+            var fileinputs = $("<div>").addClass("fileinputs span" + $this.attr("data-span"));
+            var fakeinput = $("<div>").addClass("fakefile");
 
-	$($.bootstrapFile);
+            var input = $("<input class='span" + ($this.attr("data-span") - 1).toString() + "' type='text' data-file='" + index + "'>");
+            var buttonFile = $("<button class='btn'>"+($this.attr("title")||"Open")+"</button>");
+            var buttonRemove = ($this.attr("data-remove") == "true") ? $("<button class='btn' style='z-index:-1'><i class='icon-remove'></i></button>") : null;
+            var classes = ["input-append", "input-prepend"];
+            var inputs = [buttonRemove, input, buttonFile];
+
+            if ($this.attr("data-orientation") == "left") { $this.removeClass("pull-right").addClass("pull-left"); classes.reverse(); inputs.reverse(); }
+            if (buttonRemove == null) { classes.splice(1, 1); }
+            fakeinput.addClass(classes.join(" "));
+            fakeinput.append(inputs);
+            $this.parent().append(fileinputs);
+            //$("<div>").addClass(classes[0]).append($("<input type='text' class='span" + ($this.attr("data-span") - 1).toString() + "'>"), $this).appendTo(fileinputs);
+            $this.appendTo(fileinputs);
+            fileinputs.append(fakeinput);
+        });
+    };
+
+    $($.bootstrapFile);
 })(jQuery);
